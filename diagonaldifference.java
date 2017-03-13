811be54da1893e9f79b199f1128132004b47528a
c Bayrak <byrock_28@hotmail.com>
2017-03-13 18:05:42 +0300

    Diagonal Difference

--- diagonaldifference.java
+++ diagonaldifference.java
@@ -8,18 +8,13 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int top=0;
-        int tmp=0;
-        int a[][] = new int[n][n];
-        for(int a_i=0; a_i < n; a_i++){
-            for(int a_j=0; a_j < n; a_j++){
-                a[a_i][a_j] = in.nextInt();
-            }
+       int n = in.nextInt();
+        in.nextLine();
+        int dif = 0;
+        for (int i = 0; i < n; i++) {
+          String[] row = in.nextLine().split(" ");
+          dif += Integer.parseInt(row[i]) - Integer.parseInt(row[n - i - 1]);
         }
-        tmp=a[0][0]+a[1][1]+a[2][2];
-        top=a[0][2]+a[1][1]+a[2][0];
-        top-=tmp;
-        System.out.println(top);
+        System.out.println(Math.abs(dif));
     }
 }
